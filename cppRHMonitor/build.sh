96553defe782c798d8868ca3a0a4f19872904411
frozepine <sonny.frozenpine@gmail.com>
2022-10-31 20:33:31 +0800

    修正库文件位置

--- cppRHMonitor/build.sh
+++ cppRHMonitor/build.sh
@@ -6,7 +6,7 @@ CURR_DIR=`pwd`
 
 BUILD_DIR="${CURR_DIR}/build"
 OUT_DIR="${CURR_DIR}/out"
-LIB_DIR="${CURR_DIR}/../../libs"
+LIB_DIR="${CURR_DIR}/../libs/rohon"
 
 [[ -d "${BUILD_DIR}" ]] && rm -rf "${BUILD_DIR}"
 [[ -d "${OUT_DIR}" ]] && rm -rf "${OUT_DIR}"
